37aaa3273d41279d8dfce9482899f3c33131c146
Sone <sthaya@gmail.com>
2020-06-08 14:07:25 -0700

    completed get requests

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -2,4 +2,38 @@ const express = require('express')
 
 const knex = require('../data/dbConfig')
 
-const router = express.Router()
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  knex
+    .select("*")
+    .from("accounts")
+    .then(accounts => {
+      res.status(200).json({data:accounts})
+    })
+    .catch(err => {
+      console.log("GET / error", err)
+
+      res.status(500).json({message: err.message})
+    })
+})
+
+router.get('/:id', (req, res) => {
+  knex
+    .select("*")
+    .from("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+      res.status(200).json({data:account})
+    })
+    .catch(err => {
+      console.log("GET / error", err)
+
+      res.status(500).json({message: err.message})
+    })
+})
+
+
+
+module.exports = router;
