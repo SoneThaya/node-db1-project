801981a39766a90c198f35fce971e738bb713400
Sone <sthaya@gmail.com>
2020-06-08 17:22:49 -0700

    finished stretch goals on sql problems

--- queries.sql
+++ queries.sql
@@ -19,5 +19,7 @@ INSERT INTO Customers ('CustomerName', 'Address', 'City', 'PostalCode', 'Country
 UPDATE Customers SET PostalCode = '111222' WHERE CustomerID = 92;
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+SELECT DISTINCT City FROM Customers;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT * FROM [Suppliers] WHERE LENGTH(SupplierName) > 20
